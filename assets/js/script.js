809ae7e6fb84597bdbca8e9d6d22b540954c40a4
mayaj0yce <m.j.thayne@gmail.com>
2023-06-07 01:04:38 -0600

    working on it moved around the buttons. had to put them back to allow functionality before sub

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,3 +147,17 @@ submitButton.addEventListener('click', showResults);
 previousButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
 }) ();
+
+// // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//   var msgInterval = setInterval(function () {
+//     // If there are no more words left in the message
+//     if (words[wordCount] === undefined) {
+//       // Use `clearInterval()` to stop the timer
+//       clearInterval(msgInterval);
+//     } else {
+//       // Display one word of the message
+//       mainEl.textContent = words[wordCount];
+//       wordCount++;
+//     }
+//   }, 1000);
+// };
